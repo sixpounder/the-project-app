43f1801a15073e157e0920439468323abe2fbeb1
Andrea <sixpounder@protonmail.com>
2018-04-28 14:46:39 +0200

    wip

---
+++ src/components/Clock.vue
@@ -0,0 +1,36 @@
+<template>
+  <span class="clock-time">{{ computedTime }}</span>
+</template>
+
+<script>
+import moment from 'moment';
+import { setInterval } from 'timers';
+
+export default {
+  data () {
+    return {
+      time: moment()
+    };
+  },
+
+  created () {
+    const vm = this;
+    setInterval(function () {
+      vm.time = moment();
+    }, 1000);
+  },
+
+  computed: {
+    computedTime ()  {
+      return this.time.format('HH:mm:ss');
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+.clock-time {
+  color: green;
+}
+</style>
