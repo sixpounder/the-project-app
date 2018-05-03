7ae288cc1266f5e4863ccadff16b4f278904a425
sixpounder <sixpounder@protonmail.com>
2018-05-03 19:34:50 +0200

    signup form

--- src/components/signup/SignupForm.vue
+++ src/components/signup/SignupForm.vue
@@ -6,8 +6,13 @@
     <form @submit.prevent="onSignupFormSubmit">
       <FormGroup>
         <label for="">Email address</label>
-        <input type="email" class="form-control" placeholder="Enter email" v-model="signup.email">
-        <small id="emailHelp" class="form-text text-danger">This email address is already in use</small>
+        <input type="email" class="form-control" placeholder="Enter email" v-model="signup.email" @change="onEmailChanged">
+        <small id="emailHelp" class="form-text text-danger" v-show="status.emailExists === true">This email address is already in use</small>
+        <small id="emailHelp" class="form-text text-success" v-show="status.emailExists === false">This email address is not yet being used</small>
+      </FormGroup>
+      <FormGroup>
+        <label for="">Choose a username</label>
+        <input type="text" class="form-control" placeholder="Enter a username" v-model="signup.identifier">
       </FormGroup>
       <FormGroup>
         <label for="">Choose a password</label>
@@ -30,6 +35,8 @@ import http from '@/lib/http';
 
 const FormGroup = () => import('@/components/common/FormGroup');
 
+let onEmailChangedDebouncedFn;
+
 export default {
   components: { FormGroup },
 
@@ -37,8 +44,13 @@ export default {
     return {
       signup: {
         email: null,
+        identifier: null,
         password: null,
         passwordConfirm: null
+      },
+
+      status: {
+        emailExists: null
       },
 
       error: null
@@ -46,20 +58,25 @@ export default {
   },
 
   created () {
-    this.checkEmail = _.debounce(() => {
-      // TODO
-    }, 800);
+    this.onEmailChanged = _.debounce(() => {
+      const vm = this;
+      http.get('/api/auth/user/check', { params: { email: this.signup.email }}).then(res => {
+        vm.status.emailExists = res.data.exists;
+      });
+    }, 800).bind(this);
   },
 
   methods: {
     onSignupFormSubmit () {
       const vm = this;
-      http.post('/api/auth/signup', this.signup).then((res) => {
-        
+      vm.status.email = null;
+      http.post('/api/auth/signup', this.signup).then(() => {
+        vm.$router.push({ name: 'home' });
+        // Show a message confirming signup
       }).catch(err => {
         vm.error = err;
       });
-    }
+    },
   }
 }
 </script>
