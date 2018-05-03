7a4743e239b4bef3cc87812dc732a68a901a8cdf
sixpounder <sixpounder@protonmail.com>
2018-05-03 20:10:45 +0200

    socket.io

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,19 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex';
 import Navbar from '@/components/Navbar';
 import '@/sass/style.scss';
 
 export default {
+  created () {
+    this.ioConnect();
+  },
+
+  methods: {
+    ...mapActions(['ioConnect'])
+  },
+  
   components: { Navbar }
 }
 </script>
--- src/store.js
+++ src/store.js
@@ -1,16 +1,32 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import Socket from 'socket.io-client/dist/socket.io.js';
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    socket: null
   },
   mutations: {
-
+    setSocket (state, instance) {
+      state.socket = instance;
+    }
   },
   actions: {
-
+    ioConnect (context) {
+      let instance = Socket('http://localhost:3000');
+      instance.on('connected', function() {
+        console.info('Websocket connected!');
+        // TODO: set auth token to identify user on subsequent requests via socket
+      }).on('disconnected', () => {
+        // TODO
+      });
+      context.commit('setSocket', instance);
+    }
+  },
+  getters: {
+    socket (state) {
+      return state.socket;
+    }
   }
 })
